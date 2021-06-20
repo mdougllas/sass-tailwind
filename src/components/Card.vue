6f181f28d2c434eff14cc541fdadc8de2dc30b62
Mike Dougllas <mdougllas@gmail.com>
2021-06-20 15:55:20 -0400

    presentation

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="card-wrapper py-3 sm:max-w-xl sm:mx-auto">
+    <div>
+        <div class="img-wrapper">
+            <img
+                class="image"
+                src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+                alt=""
+            />
+        </div>
+        <div class="info-wrapper">
+            <div class="h2-wrapper">
+                <h2>Sweet Tooth: El niño ciervo</h2>
+            </div>
+
+            <div>Series</div>
+            <div>2019</div>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+}
+</script>
